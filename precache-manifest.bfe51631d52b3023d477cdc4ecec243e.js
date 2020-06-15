8270e2ebe386f745692747ebae097467b29aa16c
ankita2497 <ankitaba140@gmail.com>
2020-06-15 12:07:13 +0530

    Updates

---
+++ precache-manifest.bfe51631d52b3023d477cdc4ecec243e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412ff893ae3d2a4d2e4038423c7b663e",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "1e695bd854c7ff18c86c",
+    "url": "/weather-react-app/static/css/main.674dad4c.chunk.css"
+  },
+  {
+    "revision": "6f9da207b79e184da8ff",
+    "url": "/weather-react-app/static/js/2.095184d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.095184d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e695bd854c7ff18c86c",
+    "url": "/weather-react-app/static/js/main.e56a9fcf.chunk.js"
+  },
+  {
+    "revision": "042d88f56819551ce146",
+    "url": "/weather-react-app/static/js/runtime-main.2c2bf34e.js"
+  },
+  {
+    "revision": "eba051cafa80948089ffe2f196f9bb31",
+    "url": "/weather-react-app/static/media/white-clouds-and-blue-sky-907485-2.eba051ca.jpg"
+  },
+  {
+    "revision": "773e4c86fb4f04daf58e9e5351319452",
+    "url": "/weather-react-app/static/media/worm-eye-view-of-trees-890049-1.773e4c86.jpg"
+  }
+]);
